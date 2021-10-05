1d2ff5aac89d2d00c044020bab2736fae682c9af
Emmanuel Lopez <jesusemm010@hotmail.com>
2021-10-05 17:35:16 -0500

    Se trae 100 imagenes las cuales se muestran en la pagina

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 const resultado = document.querySelector("#resultado");
 const formulario = document.querySelector("#formulario");
-
+resultado.style.maxWidth = "1000px";
+resultado.style.width = "90%";
 window.onload = () => {
     formulario.addEventListener("submit", validarForm);
 }
@@ -20,15 +21,41 @@ function validarForm(e){
 }
 function buscarImagenes (termino) {
     const key ="23702476-24505d11d2938c47125f9c28d";
-    const url = `https://pixabay.com/api/?key=${key}&q=${termino}`;
+    const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=100`;
     fetch(url)
         .then(response => response.json())
         .then(response => {
             mostrarImagenes(response.hits)
         })
 }
-function mostrarImagenes(){
-    console.log("here");
+function mostrarImagenes(imagenes){
+    console.log(imagenes)
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
+
+    //Iterar sobre el arreglo de imagenes y crear el html
+    imagenes.forEach( imagen =>{
+        const {previewURL, likes, views, largeImageURL} = imagen;
+
+        resultado.innerHTML += `
+        <div class="w-1/2 md:w-1/3 lg:w-1/4 mx-auto p-3 mb-4">
+            <div class="bg-white">
+                <img class="w-full" src="${previewURL}">
+                <div class="p-4">
+                    <p class="font-bold"> ${likes} <span class="font-light">Me gusta</span></p>
+                    <p class="font-bold"> ${views} <span class="font-light">Veces vista</span></p>
+                    <a
+                        class="block w-full bg-blue-800 hover:bg-blue-500 text-white uppercase font-bold text-center
+                        rounded mt-5 p-1" 
+                        href="${largeImageURL}" target="_blank" 
+                        rel="noopener noreferrer"
+                    >Ver Imagen</a>
+                </div>    
+            </div>
+        </div>
+        `;
+    });
 }
 function mostrarAlerta(mensaje){
     const existeAlerta = document.querySelector(".bg-red-100");
